adf8d3993058a322bf5cb73f435cdd04e64db994
AnaCarolinaFreitas <ana.c.freitas35@aluno.senai.br>
2025-08-26 10:42:55 -0300

    Feat: started project

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ['media.formula1.com', 'www.formula1.com', 'placehold.co'],
+      },
+};
+
+export default nextConfig;
